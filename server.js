8384ba032e363986ddbed65bc1b1a4a9bf279a9e
Vivek Sunderraj <viveksunderraj@gmail.com>
2017-08-09 01:06:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.get('/article-one',function(req,res)
 
 app.get('/article-two',function(req,res)
 {
-    res.send("Article two has been requested and response is sent");
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function(req,res)
 {
-    res.send("Article three has been requested and response is sent");
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
