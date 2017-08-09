a11a569c6afdc0c6caff7110b5526aff928f645b
Vivek Sunderraj <viveksunderraj@gmail.com>
2017-08-10 02:18:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+var nameInput=document.getElementById('name');
+var name=varInput.value;
+var submit=document.getElementById("submit_btn");
+submit.onclick=function(){
+    
+    var names=['name1','name2','name3']
+    list=""
+    for(var i=0;i<names.lenght;i++)
+    {
+        list+=<li>+names[i]+</li>
+    }
+    var ul=document.getElementById('namelist')
+    ul.innerHTML=list;
+};
